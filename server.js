a4ffe2c6691c689907fda8bcf03d8572f5132ff6
Carmine Giardino <carmine.giardino@gmail.com>
2014-03-20 11:09:35 +0100

    landingpage

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    app.use(express.static(__dirname + "/landingpage"));
+    res.sendfile('landingpage/index.html');
+
+});
+
+app.listen(8080);
