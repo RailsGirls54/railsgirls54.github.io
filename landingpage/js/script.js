86f093c721089a42e00eeba198c28a2d3ef4cb6b
david von leon <user@gorotsuki.unibz.it>
2014-03-26 12:08:40 +0100

    Layout changes and fixes

---
+++ landingpage/js/script.js
@@ -0,0 +1,38 @@
+$('body').scrollspy({target: '#header'});
+
+$(function() {
+	
+	// navigation
+	$("#navigation li a[href^='#']").on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop: $(this.hash).offset().top}, 300);
+		// edit: Opera and IE requires the "html" elm. animated
+	});
+
+	// title
+	$('#brand').hide();
+	window.onscroll = function() {
+		if (window.pageYOffset < 300)
+			$('#brand').fadeOut(150);
+		else
+			$('#brand').fadeIn(150);
+	};
+
+	// registration
+	$('#join').click(function(e) {
+		e.preventDefault();
+		var $this = $(this);
+		if($('#mc_embed_signup').is(':visible'))
+			$('#mc_embed_signup').slideUp(150, function(){
+				$this.removeClass('active');
+			});
+		else
+			$('#mc_embed_signup').slideDown(150, function(){
+				$this.addClass('active');
+				$('#mce-EMAIL').focus();
+			});
+	});
+	
+});
+
+$('#mc_embed_signup').hide();
